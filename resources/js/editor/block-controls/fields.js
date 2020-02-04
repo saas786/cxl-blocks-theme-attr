b37aa6af055a2ebbcc9198e973266d94a3f60a32
saasfreelancer <syed.sayal.shah@gmail.com>
2020-02-04 20:57:50 +0500

    cleanup

--- resources/js/editor/block-controls/fields.js
+++ resources/js/editor/block-controls/fields.js
@@ -8,32 +8,9 @@
  */
 
 // Import block controls.
-import ControlBorderRadius from './control-border-radius';
-import ControlBoxShadow    from './control-box-shadow';
-import ControlListType     from './control-list-type';
+import ControlBoxShadow from './control-box-shadow';
 
 export default [
-	{
-		name:    'listType',
-		type:    'string',
-		default: '',
-		control: ControlListType,
-		blocks:  [
-			'core/list'
-		]
-	},
-	{
-		name:    'borderRadius',
-		type:    'string',
-		default: '',
-		control: ControlBorderRadius,
-		blocks:  [
-			'core/image',
-			'core/gallery',
-			'core/media-text',
-			'core/paragraph'
-		]
-	},
 	{
 		name:    'boxShadow',
 		type:    'string',
