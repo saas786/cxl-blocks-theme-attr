2386b714f1c126bd83e3e88e43b3f50c3955aa55
saasfreelancer <syed.sayal.shah@gmail.com>
2020-02-04 20:56:36 +0500

    cleanup

--- resources/js/editor.js
+++ resources/js/editor.js
@@ -8,6 +8,5 @@
  * @license   https://www.gnu.org/licenses/gpl-2.0.html GPL-2.0-or-later
  */
 
-import blockStyles         from './editor/block-styles';
 import filterBlockEdit     from './editor/filter-block-edit';
 import filterBlockRegister from './editor/filter-block-register';
